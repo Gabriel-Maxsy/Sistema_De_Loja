006ae37f61582f4aeb4535f6b255c19de9fd3aae
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-01-14 23:18:58 -0300

    Criando modelos "Category" e "Product", Modificando Lnaguage_code e time_zone (settings.py).

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Depois:
+# picture (imagem)
+# Retirar os campos "blank" e "null" dos Product.owner pois sempre terão donos.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    name = models.CharField(max_length=50)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.PositiveIntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE, # Se o usuário for excluido, todos os produtos relacionados também serão.
+        blank=True, null=True
+    )
