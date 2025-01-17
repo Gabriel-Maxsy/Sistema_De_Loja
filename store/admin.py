e1f03826802f9278e9eb0e79888988e5cbe1c407
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-01-16 23:50:04 -0300

    Customizando a visualização e opções dos nossos modelos na área administrativa Django (/admin).

--- store/admin.py
+++ store/admin.py
@@ -5,7 +5,14 @@ from store import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'name', 'category', 'price',
+    ordering = '-id',
+    # list_filter = 'created_date',
+    search_fields = 'id', 'name',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'price',
+    list_display_links = 'id', 'name',
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
