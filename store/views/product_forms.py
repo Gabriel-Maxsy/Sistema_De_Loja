78b3b7052b1c40ebbb3610715ef679c979cbea53
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-02-04 23:46:21 -0300

    Usando request.method e request.POST para saber quando o formulário é postado.

--- store/views/product_forms.py
+++ store/views/product_forms.py
@@ -4,9 +4,20 @@ from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
 
 def create(request):
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('name'))
+        print(request.POST.get('description'))
+        print()
+
     context = {
 
     }
+
+    print()
+    print(request.method)
+    print()
 
     return render(
         request,
