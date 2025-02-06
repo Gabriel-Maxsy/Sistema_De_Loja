2eea24b6243b2a5ddf9e0128c213bd3d387a405a
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-02-05 23:58:45 -0300

    Criando um formulário dinâmico com forms.ModelForm.

--- store/views/product_forms.py
+++ store/views/product_forms.py
@@ -2,22 +2,31 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from store.models import Product
+from django import forms
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = (
+            'name', 'price', 'description', 'price', 'amount',
+        )
 
 def create(request):
     if request.method == 'POST':
-        print()
-        print(request.method)
-        print(request.POST.get('name'))
-        print(request.POST.get('description'))
-        print()
+
+        context = {
+            'form': ProductForm(request.POST)
+        }
+
+        return render(
+            request,
+            'store/create.html',
+            context
+        )
 
     context = {
-
+        'form': ProductForm()
     }
-
-    print()
-    print(request.method)
-    print()
 
     return render(
         request,
