30c49e27b7620145a3704e13d83a2004265c0b96
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-02-12 23:04:12 -0300

    Configurando os campos e widgets do formulário.

--- store/forms.py
+++ store/forms.py
@@ -3,11 +3,39 @@ from django import forms
 from . import models
 
 class ProductForm(forms.ModelForm):
+
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Aqui veio do init',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init',
+        # })
+
     class Meta:
         model = models.Product
         fields = (
             'name', 'price', 'description', 'price', 'amount',
         )
+
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
