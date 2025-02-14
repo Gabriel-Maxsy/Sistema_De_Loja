11b58aa24f5fe6d8ec0ec33d9cfb1bfbc7edad0e
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-02-13 23:26:53 -0300

    clean, clean_field, add_error e ValidationError.

--- store/forms.py
+++ store/forms.py
@@ -17,7 +17,7 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.fields['first_name'].widget.attrs.update({
+        # self.fields['name'].widget.attrs.update({
         #     'class': 'classe-a classe-b',
         #     'placeholder': 'Aqui veio do init',
         # })
@@ -25,7 +25,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = models.Product
         fields = (
-            'name', 'price', 'description', 'price', 'amount',
+            'name', 'price', 'description', 'amount',
         )
 
         # widgets = {
@@ -38,22 +38,31 @@ class ProductForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        price = cleaned_data.get('price')
+        amount = cleaned_data.get('amount')
 
-        self.add_error(
-            'name',
-            ValidationError(
-                'Mensagem de erro',
+        if price == amount:
+            msg = ValidationError(
+                'Preço não pode ser igual a quantidade',
                 code='invalid'
             )
-        )
 
-        self.add_error(
-            'name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+            self.add_error('price', msg)
+            self.add_error('amount', msg)
         
         return super().clean()
+    
+    def clean_name(self):
+       name = self.cleaned_data.get('name')
+
+       if name == 'ABC':
+           self.add_error(
+               'name',
+               ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+        
+       return name
