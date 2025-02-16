925e41d9d064c742ea933ec10e0a8cf2165dd351
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-02-16 20:45:19 -0300

    Usando instance do ModelForm para atualizar dados de um contato.

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search/', views.search, name='search'), 
 
     # Product CRUD 
-    path('<int:product_id>/', views.product, name='product'), # Read
-    path('product/create/', views.create, name='create') # Create
+    path('product/<int:product_id>/', views.product, name='product'), # Read
+    path('product/create/', views.create, name='create'), # Create
+    path('product/<int:product_id>/update/', views.update, name='update'), # Update
 ]
--- store/views/product_forms.py
+++ store/views/product_forms.py
@@ -1,17 +1,22 @@
 from store.forms import ProductForm
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse
+from store.models import Product
 
 def create(request):
+    form_action = reverse('store:create')
+
     if request.method == 'POST':
         form = ProductForm(request.POST)
 
         context = {
-            'form': form 
+            'form': form,
+            'form_action': form_action, 
         }
 
         if form.is_valid():
-            form.save()
-            return redirect('store:create')
+            product = form.save()
+            return redirect('store:update', product_id=product.pk)
 
         return render(
             request,
@@ -20,7 +25,43 @@ def create(request):
         )
 
     context = {
-        'form': ProductForm()
+        'form': ProductForm(),
+        'form_action': form_action,
+    }
+
+    return render(
+        request,
+        'store/create.html',
+        context
+    )
+
+def update(request, product_id):
+    product = get_object_or_404(
+        Product, pk=product_id, 
+    )
+    form_action = reverse('store:update', args=(product_id,))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+
+        context = {
+            'form': form,
+            'form_action': form_action, 
+        }
+
+        if form.is_valid():
+            product = form.save()
+            return redirect('store:update', product_id=product.pk)
+
+        return render(
+            request,
+            'store/create.html',
+            context
+        )
+
+    context = {
+        'form': ProductForm(instance=product),
+        'form_action': form_action,
     }
 
     return render(
